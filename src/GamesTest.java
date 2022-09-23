cf4df4ba6e8fc5442d1ce14a359950690c29bc72
Simon Ljungström <simonlj8@gmail.com>
2022-09-23 22:10:06 +0200

    Set up a test template

---
+++ src/GamesTest.java
@@ -0,0 +1,63 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author simon
+ *
+ */
+class GamesTest {
+
+	/**
+	 * Test method for {@link Game#Game()}.
+	 */
+	@Test
+	void testGame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Game#main(java.lang.String[])}.
+	 */
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Game#paint(java.awt.Graphics)}.
+	 */
+	@Test
+	void testPaintGraphics() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Game#score()}.
+	 */
+	@Test
+	void testScore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Game#gameOver()}.
+	 */
+	@Test
+	void testGameOver() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Game#welcomMessage()}.
+	 */
+	@Test
+	void testWelcomMessage() {
+		fail("Not yet implemented");
+	}
+
+}
